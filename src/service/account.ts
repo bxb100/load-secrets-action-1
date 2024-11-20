b8fd4ab0987791135a72147ae14341ef5dcb8838
Lemon <bxb100@outlook.com>
2024-11-20 19:58:51 +0800

    feat: rewrite to pure js

---
+++ src/service/account.ts
@@ -0,0 +1,35 @@
+import type { ResolveSecretReference } from "./types";
+import { Client, createClient } from "@1password/sdk";
+import process from "node:process";
+import { envServiceAccountToken } from "../constants";
+import assert from "node:assert";
+import { version } from "../../package.json";
+
+export class Account implements ResolveSecretReference {
+	private readonly token: string;
+	private client?: Client;
+
+	constructor() {
+		assert(
+			process.env[envServiceAccountToken],
+			`${envServiceAccountToken} is required`,
+		);
+		this.token = process.env[envServiceAccountToken];
+	}
+
+	async init(): Promise<Client> {
+		if (!this.client) {
+			this.client = await createClient({
+				auth: this.token,
+				integrationName: "1Password GitHub Action",
+				integrationVersion: version,
+			});
+		}
+		return this.client;
+	}
+
+	async resolve(ref: string): Promise<string> {
+		const client = await this.init();
+		return client.secrets.resolve(ref);
+	}
+}
